9b6ca78ac34b33ecf3cae65134bdefdc84fb50cb
OfficiallyXenos <akinpelutomiwa001@gmail.com>
2025-12-08 16:40:47 +0100

    Phase 1 Complete: Project Setup and Spotify auth

---
+++ src/spotify_auth.py
@@ -0,0 +1,17 @@
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=os.getenv("SPOTIFY_CLIENT_ID"),
+    client_secret=os.getenv("SPOTIFY_CLIENT_SECRET"),
+    redirect_uri=os.getenv("SPOTIFY_REDIRECT_URI"),
+    scope="user-read-recently-played"
+))
+
+me = sp.me()
+print(me["display_name"])
